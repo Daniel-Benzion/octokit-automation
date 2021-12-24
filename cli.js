2cdb1e3f6a6d802797d37a58629804e23f534575
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-12-24 12:17:56 -0600

    update request

--- cli.js
+++ cli.js
@@ -1,4 +1,4 @@
-import { Octokit } from 'octokit';
+const { Octokit } =  require('octokit');
 
 const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN, 
@@ -11,3 +11,11 @@ async function run() {
 }
 
 run();
+
+function bumpBoopCounter(content) {
+    return content.replace(
+        /<!-- boop-counter -->(\d+)<!-- \/boop-counter -->/,
+        (_content, counter) =>
+            `<!-- boop-counter -->${Number(counter) + 1}<!-- /boop-counter -->`,
+    );
+}
